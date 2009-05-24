45373a0f09afb9ed0d15adab8ee2a1dd94fb7d0b
turo.cabrera <turo.cabrera@67d59b7c-46d9-11de-b083-b7e289dd2314>
2009-05-24 21:35:12 +0000

    se agregaron clases daos y services

---
+++ negocio/logica/src/main/java/ar/com/futbolprode/negocio/logica/service/impl/TorneoServiceImpl.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package ar.com.futbolprode.negocio.logica.service.impl;
+
+import java.util.List;
+
+import ar.com.futbolprode.negocio.entidades.modelo.Torneo;
+import ar.com.futbolprode.negocio.logica.dao.TorneoDao;
+import ar.com.futbolprode.negocio.logica.service.TorneoService;
+
+/**
+ * @author acabrera
+ * 
+ */
+public class TorneoServiceImpl implements TorneoService {
+
+	protected TorneoDao torneoDao;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ar.com.futbolprode.negocio.logica.service.TorneoService#getAll()
+	 */
+	@Override
+	public List<Torneo> getAll() {
+		return this.torneoDao.getAll();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ar.com.futbolprode.negocio.logica.service.TorneoService#getById(java.
+	 * lang.Integer)
+	 */
+	@Override
+	public Torneo getById(Integer id) {
+		return this.torneoDao.getById(id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seear.com.futbolprode.negocio.logica.service.TorneoService#save(ar.com.
+	 * futbolprode.negocio.entidades.modelo.Torneo)
+	 */
+	@Override
+	public void save(Torneo torneo) {
+		this.torneoDao.save(torneo);
+
+	}
+
+	public TorneoDao getTorneoDao() {
+		return torneoDao;
+	}
+
+	public void setTorneoDao(TorneoDao torneoDao) {
+		this.torneoDao = torneoDao;
+	}
+
+}
