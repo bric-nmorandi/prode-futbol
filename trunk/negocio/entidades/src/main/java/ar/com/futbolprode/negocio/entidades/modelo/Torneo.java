0a435236b13927d2f77eb00beb45a9a049e67bdc
turo.cabrera <turo.cabrera@67d59b7c-46d9-11de-b083-b7e289dd2314>
2009-05-25 02:39:32 +0000

    cambio en la clase torneo

--- trunk/negocio/entidades/src/main/java/ar/com/futbolprode/negocio/entidades/modelo/Torneo.java
+++ trunk/negocio/entidades/src/main/java/ar/com/futbolprode/negocio/entidades/modelo/Torneo.java
@@ -17,10 +17,6 @@ public class Torneo extends Auditor {
 	 * nombre del torneo
 	 */
 	private String nombre;
-	/**
-	 * lista de los clubese que participan en el torneo
-	 */
-	private List<Club> clubes;
 
 	public String getNombre() {
 		return nombre;
@@ -28,16 +24,5 @@ public class Torneo extends Auditor {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}
-
-	public List<Club> getClubes() {
-		return clubes;
-	}
-
-	public void setClubes(List<Club> clubes) {
-		if (this.clubes == null) {
-			this.clubes = new ArrayList<Club>();
-		}
-		this.clubes = clubes;
 	}
 }
