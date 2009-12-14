d1f3ed8db3acc57a18b1c35b8c012a22767f676f
turo.cabrera <turo.cabrera@67d59b7c-46d9-11de-b083-b7e289dd2314>
2009-12-14 18:35:05 +0000

    cambios en la capa de negocio

---
+++ negocio/logica/src/main/java/ar/com/futbolprode/negocio/logica/service/impl/LigaServiceImpl.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package ar.com.futbolprode.negocio.logica.service.impl;
+
+import java.util.List;
+
+import ar.com.futbolprode.negocio.entidades.modelo.liga.Liga;
+import ar.com.futbolprode.negocio.logica.dao.LigaDao;
+import ar.com.futbolprode.negocio.logica.service.LigaService;
+
+/**
+ * @author acabrera
+ * 
+ */
+public class LigaServiceImpl implements LigaService {
+
+	private LigaDao ligaDao;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ar.com.futbolprode.negocio.logica.service.LigaService#getAll()
+	 */
+	@Override
+	public List<Liga> getAll() {
+		return this.ligaDao.getAll();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ar.com.futbolprode.negocio.logica.service.LigaService#getById(java.lang
+	 * .Integer)
+	 */
+	@Override
+	public Liga getById(Integer id) {
+		return this.ligaDao.getById(id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ar.com.futbolprode.negocio.logica.service.LigaService#save(ar.com.futbolprode
+	 * .negocio.entidades.modelo.liga.Liga)
+	 */
+	@Override
+	public void save(Liga liga) {
+		this.ligaDao.save(liga);
+
+	}
+
+	public LigaDao getLigaDao() {
+		return ligaDao;
+	}
+
+	public void setLigaDao(LigaDao ligaDao) {
+		this.ligaDao = ligaDao;
+	}
+
+}
