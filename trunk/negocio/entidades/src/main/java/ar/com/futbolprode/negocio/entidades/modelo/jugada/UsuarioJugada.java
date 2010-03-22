791bde183f8883e5bfaf5ed7d3b34bb28b5bd069
turo.cabrera <turo.cabrera@67d59b7c-46d9-11de-b083-b7e289dd2314>
2010-03-22 00:29:56 +0000

    se agrego metodos de jugada de cada usuario

---
+++ trunk/negocio/entidades/src/main/java/ar/com/futbolprode/negocio/entidades/modelo/jugada/UsuarioJugada.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package ar.com.futbolprode.negocio.entidades.modelo.jugada;
+
+import ar.com.futbolprode.commons.auditor.Auditor;
+import ar.com.futbolprode.negocio.entidades.modelo.usuario.Usuario;
+
+/**
+ * @author Veronica
+ *
+ */
+public class UsuarioJugada extends Auditor {
+
+	/**
+	 * {@link Usuario}
+	 */
+	private Usuario usuario;
+	
+	/**
+	 * {@link Jugada}
+	 */
+	private Jugada jugada;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Jugada getJugada() {
+		return jugada;
+	}
+	public void setJugada(Jugada jugada) {
+		this.jugada = jugada;
+	}
+	
+}
---
+++ trunk/negocio/logica/src/main/java/ar/com/futbolprode/negocio/logica/dao/JugadaDao.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package ar.com.futbolprode.negocio.logica.dao;
+
+import java.util.List;
+
+import ar.com.futbolprode.negocio.entidades.modelo.jugada.Jugada;
+
+/**
+ * @author Veronica
+ *
+ */
+public interface JugadaDao {
+
+	/**
+	 * 
+	 * @param jugada
+	 */
+    void save(Jugada jugada);
+    /**
+     * 
+     * @param integer
+     * @return
+     */
+    Jugada getById(Integer integer);
+    /**
+     * 
+     * @return
+     */
+    List<Jugada> getAll();
+}
