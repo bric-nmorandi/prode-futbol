86747ab4522473bba9b59f6abc20391b90cd3c17
turo.cabrera <turo.cabrera@67d59b7c-46d9-11de-b083-b7e289dd2314>
2010-05-29 02:59:21 +0000

    1-nuevos cambios en la capa de vista

--- trunk/web/cliente/src/main/java/ar/com/futbolprode/web/cliente/bean/JugadaBean.java
+++ trunk/web/cliente/src/main/java/ar/com/futbolprode/web/cliente/bean/JugadaBean.java
@@ -3,10 +3,56 @@
  */
 package ar.com.futbolprode.web.cliente.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.com.futbolprode.negocio.entidades.modelo.jugada.Jugada;
+import ar.com.futbolprode.negocio.entidades.modelo.usuario.Usuario;
+
 /**
  * @author acabrera
  *
  */
 public class JugadaBean extends AdministrarBean {
 
+	public List<Jugada> jugadas;
+
+	public Usuario usuario;
+	@Override
+	protected void init() {
+		this.populatedListaJugadas();
+	}
+	private void populatedListaJugadas(){
+		this.usuario=this.getUsuarioService().getById(1);
+		procesarSetAListaJugada();
+//		OBTENER jugadas de acuerdo a un usuario.		
+	}
+	private void procesarSetAListaJugada(){
+		for (Jugada jugada : this.usuario.getJugadas()) {
+			this.getJugadas().add(jugada);
+		}
+	}
+	public List<Jugada> getJugadas() {
+		if(this.jugadas==null){
+			this.jugadas=new ArrayList<Jugada>();
+		}
+		return jugadas;
+	}
+
+	public void setJugadas(List<Jugada> jugadas) {
+		if(this.jugadas==null){
+			this.jugadas=new ArrayList<Jugada>();
+		}
+		this.jugadas = jugadas;
+	}
+	
+	public void modificarJugada(){
+//		
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
